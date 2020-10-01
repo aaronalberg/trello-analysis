622d0d848ca797f859d22213a71e88d8b613bf9f
aaronalberg <aalberg2@illinois.edu>
2020-09-30 23:01:07 -0500

    fixed data printing to console

--- trello.py
+++ trello.py
@@ -8,9 +8,6 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 API_TOKEN = os.getenv('API_TOKEN')
 BOARD_ID = os.getenv('BOARD_ID')
-print(API_KEY)
-print(API_TOKEN)
-
 
 def api(endpoint):
     boardId = BOARD_ID
@@ -35,21 +32,34 @@ for l in api("lists"):
     list_map[l["id"]] = l["name"]
 
 
-#print(list_map)
-
+cardCount = 0
 list_to_card = {}
 for c in api("cards"):
+    cardCount += 1
     if c["idList"] in list_to_card:
         list_to_card[c["idList"]].append(c)
     else:
         list_to_card[c["idList"]] = [c]
 
+print("""--------
+SUMMARY""")
+print("NUMBER OF LISTS: " + str(len(list_to_card)))
+print("TOTAL NUMBER OF CARDS: " + str(cardCount))
+print("--------")
+
 for l in list_to_card:
-    print(list_map[l])
-    print(len(list_to_card[l]))
+    print("NAME: \"" + list_map[l] + "\"")
+    print("COUNT: " + str(len(list_to_card[l])))
+    print()
+
+
+    #Uncomment to also print all cards on board, by list
     '''
+    print("CARDS: ")
     for c in list_to_card[l]:
-        print("\n" + c["name"])
-        '''
+        print("\n" + "- " + c["name"])
+    print()
+    print("--------------")
+    '''
 
 
